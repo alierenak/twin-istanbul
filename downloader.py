9e3ec9f4941b9df2c06708339b72c633504a44f6
Ali Eren Ak <akali@sabanciuniv.edu>
2022-05-05 20:47:19 +0300

    data downloader added.

--- downloader.py
+++ downloader.py
@@ -29,7 +29,22 @@ dataset_links = {
         "hourly_transportation_202105.csv": "https://data.ibb.gov.tr/dataset/a6855ce7-4092-40a5-82b5-34cf3c7e36e3/resource/42b4a830-fee6-4765-9daf-3f3884f09b4b/download/hourly_transportation_202105.csv",
     },
     "Railway station-based data": {
-        'railway_station_based_data.csv': "https://data.ibb.gov.tr/tr/dataset/ae3b2e4b-073a-48d0-8ef3-f28f19bcb19c/resource/604776d6-e99f-469c-bf25-25ccadc5e89b/download/rayl-sistemler-istasyon-bazl-yolcu-ve-yolculuk-saylar.csv"
+        "railway_station_based_data.csv": "https://data.ibb.gov.tr/tr/dataset/ae3b2e4b-073a-48d0-8ef3-f28f19bcb19c/resource/604776d6-e99f-469c-bf25-25ccadc5e89b/download/rayl-sistemler-istasyon-bazl-yolcu-ve-yolculuk-saylar.csv"
+    },
+    "District and Social Economical Scores": {
+        "district_and_building_counts.csv": "https://data.ibb.gov.tr/tr/dataset/be3582eb-09d7-42f8-84d3-b3817dc9ab0a/resource/cef193d5-0bd2-4e8d-8a69-275c50288875/download/2017-yl-mahalle-bazl-bina-saylar.csv",
+        "ses_scores.xlsx": "https://data.ibb.gov.tr/tr/dataset/96e8a63a-649b-4b4b-86e1-5bd2481ac04c/resource/859f3f2d-d06e-4f1c-ba3d-5fcf89744048/download/2016-yl-mahallem-istanbul-ses-skorlar.xlsx",
+    },
+    "Traffic Index Data": {
+        "daily_traffic_index.csv": "https://data.ibb.gov.tr/tr/dataset/b3fbb6ce-03a5-4777-8c6c-111c73775523/resource/ba47eacb-a4e1-441c-ae51-0e622d4a18e2/download/traffic_index.csv"
+    },
+    "GTFS Data": {
+        "gtfs_calendar.csv": "https://data.ibb.gov.tr/tr/dataset/121a9892-7945-419a-9b89-49f6083926df/resource/c84ca913-29ac-4f15-87cd-076aef3dccd6/download/calendar.csv",
+        "gtfs_routes.csv": "https://data.ibb.gov.tr/tr/dataset/121a9892-7945-419a-9b89-49f6083926df/resource/36b554c7-cae0-4b7e-978f-fc6a43664e88/download/routes.csv",
+        "gtfs_shapes.csv": "https://data.ibb.gov.tr/tr/dataset/121a9892-7945-419a-9b89-49f6083926df/resource/83317085-aa56-41b0-9447-ea579567f2cb/download/shapes.csv",
+        "gtfs_stop_times.csv": "https://data.ibb.gov.tr/tr/dataset/121a9892-7945-419a-9b89-49f6083926df/resource/ac646b83-3b6f-4ca2-afb4-9071ab44d9af/download/stop_times.csv",
+        "gtfs_stops.csv": "https://data.ibb.gov.tr/tr/dataset/121a9892-7945-419a-9b89-49f6083926df/resource/d1f7c258-bbc1-406f-9ab2-7a7c1797c673/download/stops.csv",
+        "gtfs_trips.csv": "https://data.ibb.gov.tr/tr/dataset/121a9892-7945-419a-9b89-49f6083926df/resource/dcee1700-e59f-4a5f-8009-f602045a4507/download/trips.csv"
     }
 
 }
@@ -42,7 +57,7 @@ def download_dataset():
         logging.info(f"{dataset} is downloading...")
         for path, url in info.items():
             if not os.path.isfile(f'./data/{path}'):
-                filename = wget.download(url, out="./data")
+                filename = wget.download(url, out=f"./data/{path}")
                 logging.info(f"{filename} is downloaded.")
             else:
                 logging.warning(f'{path} was already downloaded. Skipping...')
